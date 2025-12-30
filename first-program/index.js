f421a64b05ed1b12bdc86ae8c2933741dbb6218b
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-12-30 21:39:40 +0600

    feat:basic code

---
+++ first-program/index.js
@@ -0,0 +1,9 @@
+var userName;
+userName = 'Tanjila';
+console.log(userName);
+//
+function addNumbers(num1, num2) {
+    console.log(num1 + num2);
+}
+addNumbers(20, 30);
+// tsc index.ts -> index.js -> node index.js
---
+++ first-program/index.ts
@@ -0,0 +1,16 @@
+let userName: string;
+userName = 'Tanjila'
+console.log(userName);
+
+//
+function addNumbers(num1: number, num2:number){
+    console.log(num1 + num2);
+}
+addNumbers(20,30);
+
+
+
+
+
+
+// tsc index.ts -> index.js -> node index.js
