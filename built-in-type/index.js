5196daea954d6d159b2d534cd8a3d6a1203707b6
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-01-01 21:29:52 +0600

    feat: built-in-type

---
+++ built-in-type/index.js
@@ -0,0 +1,12 @@
+//built-in-data type: number, string, boolean, void, undefined, null
+var userId;
+var firstName;
+var lastName;
+var fullName;
+var isActivated;
+userId = 101;
+firstName = 'Tanjila';
+lastName = ' Khatun';
+isActivated = true;
+fullName = firstName.concat(lastName);
+console.log("Your id: ".concat(userId, ", userName:").concat(fullName, ", account activated: ").concat(isActivated));
---
+++ built-in-type/index.ts
@@ -0,0 +1,16 @@
+//built-in-data type: number, string, boolean, void, undefined, null
+
+let userId: number;
+let firstName: string;
+let lastName: string;
+let fullName: string;
+let isActivated: boolean;
+
+userId = 101;
+firstName = 'Tanjila';
+lastName = ' Khatun';
+isActivated = true;
+
+fullName = firstName.concat(lastName);
+
+console.log(`Your id: ${userId}, userName:${fullName}, account activated: ${isActivated}`);
