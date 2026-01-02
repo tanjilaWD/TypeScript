d1a86b178170f799a121c119d21286625ee36589
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-01-02 22:08:09 +0600

    feat:tuple data type

---
+++ tuple-data-type/index.js
@@ -0,0 +1,5 @@
+// mixed data - key, value
+var user;
+user = [101, 'abc'];
+user.push(120, 'mim');
+console.log(user);
---
+++ tuple-data-type/index.ts
@@ -0,0 +1,6 @@
+// mixed data - key, value
+let user: [number, string];
+user = [101, 'abc'];
+user.push(120, 'mim');
+
+console.log(user);
