9432fa8e94ec2b740f31a1a796604b7c3597e489
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-01-02 21:41:22 +0600

    feat:array type

---
+++ arry-type/index.js
@@ -0,0 +1,7 @@
+// let userNames: string[];
+var userNames;
+userNames = ['tanjila', 'bithi'];
+console.log(userNames[0]);
+//
+var multiplyTypes;
+multiplyTypes = [1, 'name'];
---
+++ arry-type/index.ts
@@ -0,0 +1,8 @@
+// let userNames: string[];
+let userNames: Array<string>;
+
+userNames = ['tanjila','bithi'];
+console.log(userNames[0]);
+//
+let multiplyTypes : (string | number) [];
+multiplyTypes = [1, 'name'];
