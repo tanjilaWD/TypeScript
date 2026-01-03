0512ac39729a24eedacf98d4b2ef0e1211c0bfd6
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-01-03 18:16:38 +0600

    feat:enum data type

---
+++ Enum-data-type/index.js
@@ -0,0 +1,28 @@
+// enum - store constants; duplicate value is not allowed here
+//enum types: numeric, string, hetergenous
+//numeric enum
+var RequestType;
+(function (RequestType) {
+    RequestType[RequestType["readData"] = 1] = "readData";
+    RequestType[RequestType["saveData"] = 3] = "saveData";
+    RequestType[RequestType["deleteData"] = 4] = "deleteData";
+})(RequestType || (RequestType = {}));
+// console.log(RequestType);
+// console.log(RequestType.readData);
+// console.log(RequestType['readData']);
+//string enum
+var RequestType2;
+(function (RequestType2) {
+    RequestType2["readData"] = "READ_DATA";
+    RequestType2["deleteData"] = "DELETE_DATA";
+})(RequestType2 || (RequestType2 = {}));
+// console.log(RequestType2.deleteData);
+// console.log(RequestType2['deleteData']);
+//hetergenous enum
+var RequestType3;
+(function (RequestType3) {
+    RequestType3["readData"] = "READ_DATA";
+    RequestType3["deleteData"] = "DELETE_DATA";
+    RequestType3[RequestType3["id"] = 101] = "id";
+})(RequestType3 || (RequestType3 = {}));
+console.log(RequestType3);
