004ad27cb6abae86b793c52ddfe5c5a800124ecf
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-01-03 19:05:48 +0600

    feat:object-data-type

---
+++ object-data-type/index.js
@@ -0,0 +1,15 @@
+var user1;
+user1 = { userName: 'tanjila' };
+console.log(user1);
+//? optional
+//
+var users;
+users = [{ name: 'bithi' }, { name: 'mim' }];
+console.log(users);
+//
+var userss;
+userss = [];
+var user11;
+user11 = { userName: 'feeha', userId: 111 };
+userss.push(user11);
+console.log(userss);
---
+++ object-data-type/index.ts
@@ -0,0 +1,18 @@
+let user1 : { userName: string, userId?: number};
+ user1 = { userName: 'tanjila'};
+console.log(user1);
+//? optional
+
+//
+let users: object[];
+users = [{name: 'bithi'},{name: 'mim'}];
+console.log(users);
+
+//
+let userss: object[];
+userss = [];
+
+let user11:{userName: string, userId: number};
+user11 ={userName:'feeha', userId:111};
+userss.push(user11);
+console.log(userss);
