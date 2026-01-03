0e5fb481e1393c260a750d47cb8676edf63442ee
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-01-03 18:35:14 +0600

    feat: any type data

---
+++ any-type/index.js
@@ -0,0 +1,6 @@
+var userName;
+userName = 'tanjila',
+    userName = 121;
+userName = true;
+userName = [10, 20];
+console.log(userName);
---
+++ any-type/index.ts
@@ -0,0 +1,7 @@
+let userName: any;
+userName = 'tanjila',
+userName = 121;
+userName = true;
+userName = [10,20];
+
+console.log(userName);
