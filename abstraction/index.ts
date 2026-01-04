41991a0eb8df45d573df71351e7ab75bdb5e2c95
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-01-04 18:03:12 +0600

    feat: abstraction

---
+++ abstraction/index.ts
@@ -0,0 +1,25 @@
+abstract class User{
+ userName: string;
+ age: number;
+
+constructor(userName: string, age: number){
+   this.userName = userName;
+   this.age = age;
+}
+abstract display():void;
+}
+class Student extends User{
+    studentId: number;
+
+    constructor(userName: string, age: number, studentId: number){
+    super(userName, age);
+    this.studentId = studentId;
+}
+display():void{
+    console.log(`userName: ${this.userName}, age: ${this.age}, id: ${this.studentId}`);
+
+}
+}
+let student1 = new Student('Tanjila', 22, 6358493);
+student1.display();
+
